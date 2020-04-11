2fed5e5b4b01c15a5f5a06c0d47fee95971a5587
Ilja Strobbe <ilja@robonaut.be>
2020-04-11 12:21:12 +0200

    Add typescript declaration file

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare class Avg {
+  calculate(
+    input: Record<string, number | string> | Array<string | number>
+  ): Record<string, number>;
+  calculate(
+    input: Record<string, number | string> | Array<string | number>
+  ): Record<string, number>;
+}
+
+export = Avg;
--- test/avg.js
+++ test/avg.js
@@ -1,6 +1,6 @@
-global.assert = require("chai").assert;
-global.expect = require("chai").expect;
-const Averages = require("../lib");
+global.assert = require('chai').assert;
+global.expect = require('chai').expect;
+const Averages = require('../lib');
 
 //normal rounding would result in a rounded sum of 99
 const fieldValues = {
@@ -12,8 +12,8 @@ const fieldValues = {
   5: 973645,
 };
 
-describe("Averages", () => {
-  it("rounds averages", () => {
+describe('Averages', () => {
+  it('rounds averages', () => {
     const averages = new Averages();
     const roundedAverages = averages.calculate(fieldValues);
     let sum = 0;
@@ -23,17 +23,23 @@ describe("Averages", () => {
     expect(sum).to.equal(100);
   });
 
-  it("0 field values", function () {
+  it('0 field values', function () {
     const averages = new Averages();
     const roundedAverages = averages.calculate({ a: 0, b: 0, c: 0 });
-    expect(roundedAverages).to.have.property("a").that.eql(0);
-    expect(roundedAverages).to.have.property("b").that.eql(0);
-    expect(roundedAverages).to.have.property("c").that.eql(0);
+    expect(roundedAverages).to.have.property('a').that.eql(0);
+    expect(roundedAverages).to.have.property('b').that.eql(0);
+    expect(roundedAverages).to.have.property('c').that.eql(0);
   });
 
-  it("simple percentage", function () {
+  it('simple percentage', function () {
     const averages = new Averages();
     const roundedAverages = averages.calculate({ a: 33, b: 20, c: 50 });
     expect(roundedAverages).to.eql({ a: 32, b: 19, c: 49 });
+  });
+
+  it('accepts mixed arrays', function () {
+    const averages = new Averages();
+    const roundedAverages = averages.calculate([1, '2', 3]);
+    expect(roundedAverages).to.eql({ '0': 1, '1': 2, '2': 3 });
   });
 });
